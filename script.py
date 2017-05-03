7303381bc3497ea87f609a6a4ec676fa4830ba26
singhpardeep01 <singhpardeep01>
2017-05-03 11:39:14 -0400

    finished

--- script.py
+++ script.py
@@ -22,7 +22,61 @@ def run(filename):
     ident(tmp)
     stack = [ [x[:] for x in tmp] ]
     screen = new_screen()
-    tmp = []
+    edges = []
     step = 0.1
     for command in commands:
+        if command[0] == 'push':
+            stack.append( [x[:] for x in stack[-1]] )
+        elif command[0] == 'pop':
+            stack.pop()
+        elif command[0] == 'move':
+            t = make_translate(float(command[1]), float(command[2]), float(command[3]))
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+        elif command[0] == 'rotate':
+            theta = float(command[2]) * (math.pi / 180)
+            
+            if command[1] == 'x':
+                t = make_rotX(theta)
+            elif command[1] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+        elif command[0] == 'scale':
+            t = make_scale(float(command[1]), float(command[2]), float(command[3]))
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+        elif command[0] == 'box':
+            add_box(edges,
+                    float(command[1]), float(command[2]), float(command[3]),
+                    float(command[4]), float(command[5]), float(command[6]))
+            matrix_mult( stack[-1], edges )
+            draw_polygons(edges, screen, color)
+            edges = []
+        elif command[0] == 'sphere':
+            add_sphere(edges,
+                       float(command[1]), float(command[2]),
+                       float(command[3]), float(command[4]), step)
+            matrix_mult( stack[-1], edges )
+            draw_polygons(edges, screen, color)
+            edges = []
+        elif command[0] == 'torus':
+            add_torus(edges,
+                      float(command[1]), float(command[2]), float(command[3]),
+                      float(command[4]), float(command[0]), step)
+            matrix_mult( stack[-1], edges )
+            draw_polygons(edges, screen, color)
+            edges = []
+        elif command[0] == 'line':
+            add_edge( edges,
+                      float(command[1]), float(command[2]), float(command[3]),
+                      float(command[4]), float(command[5]), float(command[6]) )
+        elif command[0] == 'save':
+            save_extension(screen, command[0])
+        elif command[0] == 'display':
+            display(screen)
+            
         print command
+        
